f635cca6d59dd74757191dd5eb6dbd966d3d9e12
Tom Brennan <tommybrennan@gmail.com.com>
2013-11-30 12:44:21 -0500

    completed exercise 7

---
+++ ex7.rb
@@ -0,0 +1,26 @@
+puts "Mary had a little lamb."
+puts "it's fleece was white as %s." % 'snow'
+puts "And everywhere that Mary went."
+# prints 10 . characters on the screen
+puts "." * 10 # what'd that do?
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# notice how we are using pring instead of puts here.  change it to puts and see what happens
+# puts will give you cheese and burger on sepearate lines
+print end1 + end2 + end3 + end4 + end5 + end6
+print end7 + end8 + end9 +end10 + end11 + end12
+
+# this is just polite use of the terminal
+puts
