2fa7c5c5e949637498706f5a6becdde818d2b53d
Tom Brennan <tommybrennan@gmail.com.com>
2013-11-30 12:33:36 -0500

    finished exercise 6

---
+++ ex6.rb
@@ -0,0 +1,22 @@
+x = "There are #{10} types of people."
+binary = "binary"
+do_not = "don't"
+# string interplolation
+y = "Those who know #{binary} and those who #{do_not}."
+
+puts x
+puts y
+
+puts "I said: #{x}."
+puts "I also said: #{y}."
+
+hilarious = false
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+
+puts joke_evaluation
+
+w = "This is the left side of..."
+e = "A string with a right side"
+
+# string concantation
+puts w + e
